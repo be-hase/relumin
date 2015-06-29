2baddef7257650943576dd43184a9e3ead301cab
Ryosuke Hasebe <ryosuke.hasebe@linecorp.com>
2015-06-29 21:50:36 +0900

    fix pom

--- src/main/java/com/behase/relumin/service/LoggingOperationServiceImpl.java
+++ src/main/java/com/behase/relumin/service/LoggingOperationServiceImpl.java
@@ -2,7 +2,6 @@ package com.behase.relumin.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +9,6 @@ import org.springframework.stereotype.Service;
 public class LoggingOperationServiceImpl implements LoggingOperationService {
 	public static final String LOGGER_NAME = "logging_operation";
 	private static final Logger LOG = LoggerFactory.getLogger(LOGGER_NAME);
-
-	@Value("${auth.enabled}")
-	private boolean authEnabled;
 
 	@Override
 	public void log(String operationName, Authentication authentication) {
