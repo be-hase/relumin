444c5b6929c49aa49ea66ca862c85d31c9e07c9b
Ryosuke Hasebe <ryosuke.hasebe@linecorp.com>
2015-10-15 17:29:59 +0900

    fix iframe detection

--- front/src/js/app.js
+++ front/src/js/app.js
@@ -3,13 +3,16 @@ require('bootstrap');
 var React = require('react');
 var Router = require('react-router');
 var Highcharts = require('react-highcharts');
+var URI = require('urijs');
+var _ = require('lodash');
 
 var appRoutes = require('./routers/AppRoutes');
 var ClusterActions = require('./actions/ClusterActions');
 var UserActions = require('./actions/UserActions');
 
 $(function(){
-    if (window != window.parent) {
+    var getQuery = new URI().query(true);
+    if (_.has(getQuery, 'iframe')) {
         // if access from iframe
         $('head').append('<style>.dis-iframe {display: none;}</style>');
         USER = {
