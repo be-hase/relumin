8a0370f62a10eb4c509ef254eaa9bd0756c7a036
Ryosuke Hasebe <ryosuke.hasebe@linecorp.com>
2016-01-14 21:15:01 +0900

    test

--- src/test/java/com/behase/relumin/util/JedisUtilsTest.java
+++ src/test/java/com/behase/relumin/util/JedisUtilsTest.java
@@ -12,11 +12,22 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import redis.clients.jedis.Jedis;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class JedisUtilsTest {
+	@Value("${redis.host}")
+	private String redisHost;
+
+	@Value("${redis.port}")
+	private int redisPort;
+
 	@Test
 	public void getJedisByHostAndPort() {
+		Jedis jedis = new Jedis(redisHost, redisPort);
+		assertThat(jedis.ping(), is("PONG"));
 	}
 }
