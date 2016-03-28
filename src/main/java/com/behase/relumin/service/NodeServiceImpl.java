b9bc9d67011f5e2272699d904b4bad78df688e08
Ryosuke Hasebe <ryosuke.hasebe@linecorp.com>
2016-03-28 23:22:17 +0900

    write tests

--- src/main/java/com/behase/relumin/service/NodeServiceImpl.java
+++ src/main/java/com/behase/relumin/service/NodeServiceImpl.java
@@ -1,7 +1,6 @@
 package com.behase.relumin.service;
 
 import com.behase.relumin.Constants;
-import com.behase.relumin.exception.ApiException;
 import com.behase.relumin.exception.InvalidParameterException;
 import com.behase.relumin.model.ClusterNode;
 import com.behase.relumin.support.JedisSupport;
@@ -12,7 +11,6 @@ import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -44,9 +42,7 @@ public class NodeServiceImpl implements NodeService {
     @Override
     public Map<String, String> getStaticsInfo(ClusterNode clusterNode) {
         try (Jedis jedis = jedisSupport.getJedisByHostAndPort(clusterNode.getHostAndPort())) {
-            Map<String, String> result = jedisSupport.parseInfoResult(jedis.info());
-
-            return result;
+            return jedisSupport.parseInfoResult(jedis.info());
         }
     }
 
@@ -63,7 +59,7 @@ public class NodeServiceImpl implements NodeService {
         int endIndex = OFFSET;
         boolean validStart = true;
         boolean validEnd = true;
-        while (true && (validStart || validEnd)) {
+        while (validStart || validEnd) {
             log.debug("statics loop. startIndex : {}", startIndex);
             List<Map<String, String>> staticsList = getStaticsInfoHistoryFromRedis(clusterName, nodeId, fields, startIndex, endIndex);
             if (staticsList == null || staticsList.isEmpty()) {
@@ -84,29 +80,38 @@ public class NodeServiceImpl implements NodeService {
         }
         Collections.reverse(rangeResult);
 
+        long thresholdMillis = getThresholdMillis(start, end);
+        return getAveragedStaticsInfoHistory(rangeResult, fields, thresholdMillis);
+    }
+
+    long getThresholdMillis(long start, long end) {
         long durationMillis = end - start;
-        long thresholdMillis;
-        if (durationMillis <= (long) 1 * 24 * 60 * 60 * 1000) { // 1day
-            thresholdMillis = Long.MIN_VALUE;
-        } else if (durationMillis <= (long) 7 * 24 * 60 * 60 * 1000) { // 7days
-            thresholdMillis = 5 * 60 * 1000;
-        } else if (durationMillis <= (long) 30 * 24 * 60 * 60 * 1000) { // 30 days
-            thresholdMillis = 30 * 60 * 1000;
-        } else if (durationMillis <= (long) 60 * 24 * 60 * 60 * 1000) { // 60 days
-            thresholdMillis = 1 * 60 * 60 * 1000;
-        } else if (durationMillis <= (long) 120 * 24 * 60 * 60 * 1000) { // 120 days
-            thresholdMillis = 2 * 60 * 60 * 1000;
-        } else if (durationMillis <= (long) 120 * 24 * 60 * 60 * 1000) { // 180 days
-            thresholdMillis = 6 * 60 * 60 * 1000;
-        } else if (durationMillis <= (long) 365 * 24 * 60 * 60 * 1000) { // 1 years
-            thresholdMillis = 12 * 60 * 60 * 1000;
+        if (durationMillis <= (long)1 * 24 * 60 * 60 * 1000) { // 1day
+            return Long.MIN_VALUE;
+        } else if (durationMillis <= (long)7 * 24 * 60 * 60 * 1000) { // 7days
+            return 5 * 60 * 1000; // 5 min
+        } else if (durationMillis <= (long)30 * 24 * 60 * 60 * 1000) { // 30 days
+            return 30 * 60 * 1000; // 30 min
+        } else if (durationMillis <= (long)60 * 24 * 60 * 60 * 1000) { // 60 days
+            return 1 * 60 * 60 * 1000; // 1 hour
+        } else if (durationMillis <= (long)120 * 24 * 60 * 60 * 1000) { // 120 days
+            return 2 * 60 * 60 * 1000; // 2 hours
+        } else if (durationMillis <= (long)180 * 24 * 60 * 60 * 1000) { // 180 days
+            return 6 * 60 * 60 * 1000; // 6 hours
+        } else if (durationMillis <= (long)365 * 24 * 60 * 60 * 1000) { // 1 years
+            return 12 * 60 * 60 * 1000; // 12 hours
         } else {
-            thresholdMillis = 24 * 60 * 60 * 1000;
+            return 24 * 60 * 60 * 1000; // 24 hours
         }
+    }
+
+    Map<String, List<List<Object>>> getAveragedStaticsInfoHistory(List<Map<String, String>> rangeResult, List<String> fields, long thresholdMillis) {
 
         Map<String, List<List<Object>>> averageResult = Maps.newConcurrentMap();
         fields.parallelStream().forEach(field -> {
             List<List<Object>> fieldAverageResult = Lists.newArrayList();
+
+            // init data
             long preTimestamp = 0;
             BigDecimal sum = new BigDecimal(0);
             long sumTs = 0;
@@ -122,10 +127,12 @@ public class NodeServiceImpl implements NodeService {
                 }
 
                 if (preTimestamp > timestamp) {
+                    // this is invalid timestamp.
+                    // Iterate data should be sorted. Maybe, there is some noisy data.
                     continue;
                 }
 
-                if (preTimestamp == 0) {
+                if (preTimestamp == 0) { // is first. average start point.
                     preTimestamp = timestamp;
                     sum = value;
                     sumTs = timestamp;
@@ -141,33 +148,23 @@ public class NodeServiceImpl implements NodeService {
                     sumTs += timestamp;
                     count++;
                 } else {
-                    BigDecimal averageValue = new BigDecimal(0);
-                    if (count != 0) {
-                        averageValue = sum.divide(new BigDecimal(count), 4, BigDecimal.ROUND_HALF_UP);
+                    if (count > 0) {
+                        BigDecimal averageValue = sum.divide(new BigDecimal(count), 4, BigDecimal.ROUND_HALF_UP);
+                        long averageTs = sumTs / count;
+                        log.debug("ESCAPE threshold. averageValue={}, averageTs={}, sum={}, sumTs={}, sumCount={}", averageValue, averageTs, sum, sumTs, count);
+                        fieldAverageResult.add(Lists.newArrayList(averageTs, averageValue.doubleValue()));
                     }
-                    long averageTs = 0;
-                    if (count != 0) {
-                        averageTs = sumTs / count;
-                    }
-                    log.debug("ESCAPE threshold. averageValue={}, averageTs={}, sum={}, sumTs={}, sumCount={}", averageValue, averageTs, sum, sumTs, count);
-                    fieldAverageResult.add(Lists.newArrayList(averageTs, averageValue.doubleValue()));
 
-                    preTimestamp = 0;
-                    sum = new BigDecimal(0);
-                    sumTs = 0;
-                    count = 0;
+                    preTimestamp = timestamp; // average start point.
+                    sum = value;
+                    sumTs = timestamp;
+                    count = 1;
                 }
             }
 
-            if (preTimestamp != 0) {
-                BigDecimal averageValue = new BigDecimal(0);
-                if (count != 0) {
-                    averageValue = sum.divide(new BigDecimal(count), 4, BigDecimal.ROUND_HALF_UP);
-                }
-                long averageTs = 0;
-                if (count != 0) {
-                    averageTs = sumTs / count;
-                }
+            if (count > 0) {
+                BigDecimal averageValue = sum.divide(new BigDecimal(count), 4, BigDecimal.ROUND_HALF_UP);
+                long averageTs = sumTs / count;
                 log.debug("ESCAPE threshold. averageValue={}, averageTs={}, sum={}, sumTs={}, sumCount={}", averageValue, averageTs, sum, sumTs, count);
                 fieldAverageResult.add(Lists.newArrayList(averageTs, averageValue.doubleValue()));
             }
@@ -184,8 +181,8 @@ public class NodeServiceImpl implements NodeService {
             try {
                 jedis.ping();
             } catch (Exception e) {
-                log.warn("redis error.", e);
-                throw new ApiException(Constants.ERR_CODE_INVALID_PARAMETER, String.format("Failed to connect to Redis Cluster(%s). Please confirm.", hostAndPort), HttpStatus.BAD_REQUEST);
+                log.warn("Redis error.", e);
+                throw new InvalidParameterException(String.format("Failed to connect to Redis Cluster(%s). Please confirm.", hostAndPort));
             }
             jedis.shutdown();
         }
@@ -195,8 +192,8 @@ public class NodeServiceImpl implements NodeService {
                                                              List<String> fields, long startIndex, long endIndex) {
         List<Map<String, String>> result = Lists.newArrayList();
 
-        try (Jedis jedis = dataStoreJedisPool.getResource()) {
-            List<String> rawResult = jedis.lrange(Constants.getNodeStaticsInfoRedisKey(redisPrefixKey, clusterName, nodeId), startIndex, endIndex);
+        try (Jedis dataStoreJedis = dataStoreJedisPool.getResource()) {
+            List<String> rawResult = dataStoreJedis.lrange(Constants.getNodeStaticsInfoRedisKey(redisPrefixKey, clusterName, nodeId), startIndex, endIndex);
             rawResult.forEach(v -> {
                 try {
                     Map<String, String> map = mapper.readValue(v, new TypeReference<Map<String, Object>>() {
@@ -211,16 +208,14 @@ public class NodeServiceImpl implements NodeService {
         return filterGetStaticsInfoHistory(result, fields);
     }
 
-    List<Map<String, String>> filterGetStaticsInfoHistory(List<Map<String, String>> staticsInfos,
-                                                          List<String> fields) {
-        List<String> copyedFields = new ArrayList<String>(fields);
+    private List<Map<String, String>> filterGetStaticsInfoHistory(List<Map<String, String>> staticsInfos,
+                                                                  List<String> fields) {
+        List<String> copyedFields = new ArrayList<>(fields);
         copyedFields.add("_timestamp");
 
         return staticsInfos.stream().map(v -> {
             Map<String, String> item = Maps.newHashMap();
-            copyedFields.forEach(field -> {
-                item.put(field, v.get(field));
-            });
+            copyedFields.forEach(field -> item.put(field, v.get(field)));
             return item;
         }).collect(Collectors.toList());
     }
