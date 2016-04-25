a28a69ae2c8b302018799b9c79a7aebe21635c27
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-04-25 16:30:00 +0900

    add slowlogs crawl task on node scheduler

--- src/main/java/com/behase/relumin/service/NodeService.java
+++ src/main/java/com/behase/relumin/service/NodeService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import com.behase.relumin.model.ClusterNode;
+import redis.clients.util.Slowlog;
 
 public interface NodeService {
 	Map<String, String> getStaticsInfo(ClusterNode clusterNode);
+
+    List<Slowlog> getSlowLog(ClusterNode clusterNode);
 
 	Map<String, List<List<Object>>> getStaticsInfoHistory(String clusterName, String nodeId, List<String> fields,
 			long start, long end);
