03b65d10b14d2a670e70621563823136474469f8
Ryosuke Hasebe <hsb.1014@gmail.com>
2017-05-02 03:08:42 +0900

    wip

---
+++ relumin-server/src/main/java/com/be_hase/relumin/Application.java
@@ -0,0 +1,12 @@
+package com.be_hase.relumin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
